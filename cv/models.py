bb8cf202383b1825ced4b9a2570e424230d1e6cb
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-11-23 10:57:43 +0100

    Added completed

--- cv/models.py
+++ cv/models.py
@@ -28,9 +28,21 @@ class Organizations(models.model):
 class ProfressionalExperience(models.model):
   timespan = models.CharField(max_length=50)
   institution = models.CharField(max_length=50)
+  title = models.CharField(max_length=50)
   city = models.CharField(max_length=50)
   country = models.CharField(max_length=50)
+  description = models.CharField(max_length=50)
 
+class Skills(models.model):
+  skill = models.CharField(max_length=50)
+  description = models.CharField(max_length=50)
+
+class CodingProjects(models.model):
+  title = models.CharField(max_length=50)
+  tagline = models.CharField(max_length=50)
+  description = models.CharField(max_length=50)
+  live_demo = models.CharField(max_length=50)
+  repository = models.CharField(max_length=50)
 
 class Cv(models.Model):
   name = models.CharField(max_length=50)
@@ -40,6 +52,5 @@ class Cv(models.Model):
   citizenship = models.CharField(max_length=50)
   github = models.CharField(max_length=50)
   linkedin = models.CharField(max_length=50)
-  category = models.ForeignKey(Category, on_delete=models.CASCADE)
-# Create your models here.
+  introduction = models.CharField(max_length=50)
 

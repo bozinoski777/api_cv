a99e87b20c24d72ababcfa859f33fba39ec3997d
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-11-23 11:21:19 +0100

    Added Urs routesr

--- cv/urls.py
+++ cv/urls.py
@@ -4,6 +4,14 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('cv', views.CvView)
+router.register('education', views.EducationView)
+router.register('languages', views.LanguagesView)
+router.register('organizations', views.OrganizationsView)
+router.register('professionalExperience', views.ProfessionalExperienceView)
+router.register('skills', views.SkillsView)
+router.register('codingProjects', views.CodingProjectsView)
+
+
 urlpatterns = [
   path('', include(router.urls))
 ]
